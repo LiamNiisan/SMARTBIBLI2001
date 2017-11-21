e66e46cdbe379d31fcb0f51b09e77be9934a1f90
Moreauself <33062152+Moreauself@users.noreply.github.com>
2017-11-21 11:33:16 -0500

    Update t_biblio_machine.h

--- TP3_Smart_Biblio/t_biblio_machine.h
+++ TP3_Smart_Biblio/t_biblio_machine.h
@@ -1,1 +1,22 @@
 //TO BE CONTINUED
+
+#include "t_biblio_machine.c"
+
+
+typedef struct {
+
+    int no_etudiant;
+    t_livre * livre_emprunter;
+
+}t_etudiant;
+
+
+void etudiant_servir(t_etudiant * etudiant);
+
+void etudiant_chercher_livre();
+
+void etudiant_retour_livre();
+
+void etudiant_apporter_livre();
+
+void etudiant_dossier();
