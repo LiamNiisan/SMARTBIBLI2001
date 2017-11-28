323aa355b44b37d7bb5810902a8f6a168332cb0e
Moreauself <33062152+Moreauself@users.noreply.github.com>
2017-11-28 12:11:45 -0500

    Update main.c
    Ajout de la fonction Remise partielle de 15% pour la semaine prochaine - Almost Done
    Ajout du servir étudiant avec les menus et intégration d'un étudiant random pour les tests - Almost Done

--- TP3_Smart_Biblio/main.c
+++ TP3_Smart_Biblio/main.c
@@ -8,9 +8,37 @@
 #include "t_pile.h"
 
 
+void remise_partielle(t_bibliotheque * pbiblio);
+
+
 int main()
 {
-    t_etudiant lol;
-    etudiant_servir(&lol);
+    t_etudiant etudiant;
+    t_bibliotheque biblio;
+
+    //menu fonctionelle avec generation d'un user random
+    etudiant_servir(&etudiant,&biblio);
+    //remise_partielle(&biblio);
+
+
+
+
+
+
+
     return 0;
+}
+
+void remise_partielle(t_bibliotheque * pbiblio){
+
+    int lecturefichier=0; //verification de la lecture fichier
+
+    initialiser_bibliotheque(&pbiblio);
+    initialiser_rapport(&pbiblio);
+    lire_fichier(&pbiblio,&lecturefichier);
+    generer_rapport(&pbiblio);
+    emprunter_livre(&pbiblio);
+    generer_rapport(&pbiblio);
+
+
 }
