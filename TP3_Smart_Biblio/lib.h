2cbf7f30f7ac569a7c2beaec5fa3f4e1ed6c4c92
Badr <badrj48@gmail.com>
2017-12-16 13:56:56 -0500

    fichier lib.h

---
+++ TP3_Smart_Biblio/lib.h
@@ -0,0 +1,17 @@
+
+#ifndef LIB_H_INCLUDED
+#define LIB_H_INCLUDED
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "t_pile.h"
+#include "t_chaine.h"
+#include "t_biblio.h"
+#include "t_biblio_machine.h"
+#include "t_biblio_chariot.h"
+#include "t_biblio_robot.h"
+
+
+#endif // LIB_H_INCLUDED
